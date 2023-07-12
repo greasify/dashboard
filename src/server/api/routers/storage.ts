8791a8655479035ef43ec0e628e89b6a9b3b8a21
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-07-12 22:06:52 +0800

    cleanup

--- src/server/api/routers/storage.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-import { createTRPCRouter, publicProcedure } from '~/server/api/trpc'
-
-export const storageRouter = createTRPCRouter({
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    return await ctx.supabase.storage.from('registry').list()
-  })
-})
